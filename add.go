012310843adf3968a3ce1e61118f8d560d02ebd1
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2024-01-11 20:05:47 -0300

    fix message error

--- add.go
+++ add.go
@@ -5,7 +5,12 @@ import (
 )
 
 // https://crontab.guru/
+// mon:1,th:2, we:3,th:4,fr:5,sat:6,sun:0
 
+// schedule (cronograma) ej:
+// "0 7 15 1 *"  // A las 07:00 del día del mes 15 de enero
+// "0 7 * * 1,4" // 2 veces a la semana lunes y jueves
+// "*/1 * * * *" // cada 1 min
 func Add() *cronFunc {
 	return &cronFunc{
 		Crontab:    crontab.New(),
--- regitered.go
+++ regitered.go
@@ -1,16 +1,10 @@
 package cronfunc
 
-// mon:1,th:2, we:3,th:4,fr:5,sat:6,sun:0
-
-// schedule (cronograma) ej:
-// "0 7 15 1 *"  // A las 07:00 del día del mes 15 de enero
-// "0 7 * * 1,4" // 2 veces a la semana lunes y jueves
-// "*/1 * * * *" // cada 1 min
 func (c *cronFunc) AddFuncToSchedule(schedule, description string, fun any, args ...any) (err string) {
 	const e = "AddFuncToSchedule "
 
 	if descr, exist := c.registered[schedule]; exist {
-		return e + "ya existe cronograma: " + schedule + " = " + descr
+		return e + "al agregar cronograma:" + description + ", ya existe cronograma: " + schedule + " = " + descr
 	}
 
 	er := c.AddJob(schedule, fun, args...)
